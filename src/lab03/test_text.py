849505d1d737632d05bdabc7fd9da4de5c0b8f1a
alexey nikitin <63664373+PrO100mOl@users.noreply.github.com>
2025-10-19 17:21:07 +0300

    gg

---
+++ src/lab03/test_text.py
@@ -0,0 +1,25 @@
+from ..lib.text import normalize, tokenize, count_freq, top_n
+
+# normalize
+assert normalize("ПрИвЕт\nМИр\t") == "привет мир"
+assert normalize("ёжик, Ёлка") == "ежик, елка"
+
+# tokenize
+assert tokenize("привет, мир!") == ["привет", "мир"]
+assert tokenize("по-настоящему круто") == ["по-настоящему", "круто"]
+assert tokenize("2025 год") == ["2025", "год"]
+
+# count_freq + top_n
+freq = count_freq(["a","b","a","c","b","a"])
+assert freq == {"a":3,"b":2,"c":1}
+assert top_n(freq, 2) == [("a",3), ("b",2)]
+
+# тай-брейк по слову при равной частоте
+freq2 = count_freq(["bb","aa","bb","aa","cc"])
+assert top_n(freq2, 2) == [("aa",2), ("bb",2)]
+
+print("OK")
+
+
+# cd /home/mol/Desktop/python_labs                                       main        
+# python -m src.lab03.test_text
--- src/lib/text.py
+++ src/lib/text.py
@@ -1,13 +1,14 @@
 def normalize(text: str, *, casefold: bool = True, yo2e: bool = True) -> str:
-    if casefold:
-        text = text.casefold()
     
     if yo2e:
         for i in range(len(text)):
-            if text[i] == "е":
+            if text[i] == "ё":
+                text = text[:i]+"е"+text[i+1:]
+            elif text[i] == "Ё":
                 text = text[:i]+"Е"+text[i+1:]
-            elif text[i] == "ё":
-                text = text[:i]+"Ё"+text[i+1:]
+    if casefold:
+        text = text.casefold()
+    
     text = " ".join(text.split())
     return text
 
@@ -33,9 +34,9 @@ def count_freq(tokens: list[str]) -> dict[str, int]:
     freq: dict[str, int] = {}
     for t in tokens:
         if t in freq:
-            freq[t] += 1
+            freq[t.replace("ё","е")] += 1
         else:
-            freq[t] = 1
+            freq[t.replace("ё","е")] = 1
     return dict(sorted(freq.items()))
 
 
@@ -44,3 +45,21 @@ def top_n(freq: dict[str, int], n: int = 5) -> list[tuple[str, int]]:
 
 
 # Вернуть топ-N по убыванию частоты; при равенстве — по алфавиту слова.
+
+
+if __name__ == "__main__":
+    assert normalize("ПрИвЕт\nМИр\t") == "привет мир"
+    assert normalize("ёжик, Ёлка") == "ежик, елка"
+
+    assert tokenize("привет, мир!") == ["привет", "мир"]
+    assert tokenize("по-настоящему круто") == ["по-настоящему", "круто"]
+    assert tokenize("2025 год") == ["2025", "год"]
+
+    freq = count_freq(["a","b","a","c","b","a"])
+    assert freq == {"a":3,"b":2,"c":1}
+    assert top_n(freq, 2) == [("a",3), ("b",2)]
+
+    freq2 = count_freq(["bb","aa","bb","aa","cc"])
+    assert top_n(freq2, 2) == [("aa",2), ("bb",2)]
+
+    print("OK")
